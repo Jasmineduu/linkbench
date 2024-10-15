f44b821612667503250acbd317c6c988563952e0
yudu8649 <yudu8649@uni.sydney.edu.au>
2024-10-15 23:08:21 +1100

    in delete check

--- src/main/java/com/facebook/LinkBench/LinkStoreMysql.java
+++ src/main/java/com/facebook/LinkBench/LinkStoreMysql.java
@@ -304,9 +304,33 @@ public class LinkStoreMysql extends GraphStore {
   @Override
   public boolean addLink(String dbid, Link l, boolean noinverse)
       throws Exception {
+    long id1 = l.id1;
+    long id2 = l.id2;
+    String query = "select count(*) from " + nodetable + " where id = " + id1;
+    ResultSet results = stmt_ro.executeQuery(query);
+    // delete only if nums = 0 (no node in linktable)
+    int result = -1;
+    while (results.next()) {
+    result = results.getInt(1);
+    }
+    results.close();
+    // int result = results.getInt(0);
+
+    String query2 = "select count(*) from " + nodetable + " where id = " + id2;
+    ResultSet results2 = stmt_ro.executeQuery(query2);
+    // delete only if nums = 0 (no node in linktable)
+    int result2 = -1;
+    while (results2.next()) {
+    result2 = results2.getInt(1);
+    }
+    results2.close();
+
     while (true) {
       try {
-        return addLinkImpl(dbid, l, noinverse);
+        if (result != 0 && result2 != 0) {
+          return addLinkImpl(dbid, l, noinverse);
+        } else {
+          return false;
       } catch (SQLException ex) {
         if (!processSQLException(ex, "addLink")) {
           // throw ex;
@@ -1076,11 +1100,22 @@ public class LinkStoreMysql extends GraphStore {
 
   private boolean deleteNodeImpl(String dbid, int type, long id) throws Exception {
     checkNodeTableConfigured();
+    // int rows = stmt_rw.executeUpdate(
+    // "DELETE FROM `" + dbid + "`.`" + nodetable + "` " +
+    // "WHERE id=" + id + " and type =" + type + "; commit;");
     int rows = stmt_rw.executeUpdate(
         "DELETE FROM `" + dbid + "`.`" + nodetable + "` " +
-            "WHERE id=" + id + " and type =" + type + "; commit;");
+            "WHERE id=" + id + " and type =" + type + " and id not in (select id1 from "
+            + linktable
+            + " union select id2 from " + linktable + "); commit;");
+    stmt_rw.executeUpdate(
+        "DELETE FROM `" + dbid + "`.`" + counttable + "` " +
+            "WHERE id=" + id + " and id not in (select id1 from "
+            + linktable
+            + " union select id2 from " + linktable + "); commit;");
+    if (rows == 0)
 
-    if (rows == 0) {
+    {
       return false;
     } else if (rows == 1) {
       return true;
